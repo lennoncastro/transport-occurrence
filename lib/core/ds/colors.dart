72332ad39de6b1f93ccd7265ef9ba27c208fc5b6
Lennon Castro <lennon.castro.92@gmail.com>
2025-12-02 21:14:17 -0300

    feat: add bodyMedium and background color

--- lib/core/ds/colors.dart
+++ lib/core/ds/colors.dart
@@ -4,4 +4,6 @@ mixin AppColors {
   static const primary = Color(0xff006E63);
 
   static const white = Colors.white;
+
+  static const background = Color(0xffF5F6FA);
 }
--- lib/core/ds/theme.dart
+++ lib/core/ds/theme.dart
@@ -4,6 +4,7 @@ import 'package:transport_occurrence/core/ds/colors.dart';
 
 mixin AppTheme {
   static ThemeData theme = ThemeData(
+    scaffoldBackgroundColor: AppColors.background,
     fontFamily: 'Inter',
     appBarTheme: const AppBarTheme(
       centerTitle: true,
@@ -17,6 +18,15 @@ mixin AppTheme {
         fontSize: 18,
         fontWeight: FontWeight.bold,
         color: AppColors.white,
+      ),
+    ),
+    textTheme: const TextTheme(
+      bodyMedium: TextStyle(
+        fontFamily: 'Inter',
+        fontWeight: FontWeight.w400,
+        fontSize: 16,
+        height: 1.0,
+        letterSpacing: 0,
       ),
     ),
     colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
