0319c3de6a1e2ba31c95ab31e5ced36c2b6c0e73
Lennon Castro <lennon.castro.92@gmail.com>
2025-12-02 22:56:59 -0300

    feat: add labels for ocurrences page

--- lib/features/ocurrences/pages/ocurrences_page.dart
+++ lib/features/ocurrences/pages/ocurrences_page.dart
@@ -3,10 +3,33 @@ import 'package:transport_occurrence/core/ds/ds.dart';
 import 'package:transport_occurrence/core/extensions/labels.dart';
 
 class OcurrencesPage extends StatelessWidget with AppTheme, AppDimensions {
-  const OcurrencesPage({super.key});
+  OcurrencesPage({super.key});
+
+  final controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return AppPage(title: context.labels.ocurrencesPage.title);
+    final labels = context.labels.ocurrencesPage;
+    return AppPage(
+      title: labels.title,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(24),
+        child: Column(
+          children: [
+            AppTextFormField(
+              controller: controller,
+              label: labels.form.label,
+              hintText: labels.form.hintText,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return labels.form.invalidInput;
+                }
+                return null;
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
