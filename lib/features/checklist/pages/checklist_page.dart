c346608e9503f66499acd6cbd4e7e9a6923d2402
Lennon Castro <lennon.castro.92@gmail.com>
2025-12-03 20:29:39 -0300

    feat: integrate checklist with navigation service

--- lib/features/checklist/pages/checklist_page.dart
+++ lib/features/checklist/pages/checklist_page.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:transport_occurrence/core/ds/ds.dart';
 import 'package:transport_occurrence/core/extensions/labels.dart';
+import 'package:transport_occurrence/core/navigation/navigator_service.dart';
 import 'package:transport_occurrence/features/checklist/components/checklist_card.dart';
 import 'package:transport_occurrence/gen/assets.gen.dart';
 
 class ChecklistPage extends StatelessWidget {
-  const ChecklistPage({super.key});
+  ChecklistPage({super.key});
+
+  final NavigationService _navigationService = Modular.get<NavigationService>();
 
   @override
   Widget build(BuildContext context) {
@@ -19,6 +23,7 @@ class ChecklistPage extends StatelessWidget {
             SizedBox(height: AppDimensions.spacing16),
         itemBuilder: (context, index) {
           return ChecklistCard(
+            onTap: _navigationService.goToOccurrences,
             icon: SvgPicture.asset(
               Assets.icons.packagePlus,
               width: AppDimensions.icon32,
