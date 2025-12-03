30622c9193ee63498b6694edf700005083767f51
Lennon Castro <lennon.castro.92@gmail.com>
2025-12-03 19:23:47 -0300

    chore: move app to app directory

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:transport_occurrence/app.dart';
+import 'package:transport_occurrence/app/app.dart';
 import 'package:transport_occurrence/app/app_module.dart';
 
 void main() {
