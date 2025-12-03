921cf414fc42ae36ca0ddad608a2aad6b279afc9
Lennon Castro <lennon.castro.92@gmail.com>
2025-12-02 23:35:14 -0300

    feat: add more dimension values

--- lib/core/ds/dimensions.dart
+++ lib/core/ds/dimensions.dart
@@ -1,7 +1,11 @@
 mixin AppDimensions {
+  static const double spacing2 = 2;
+
   static const double spacing8 = 8;
 
   static const double spacing4 = 4;
+
+  static const double spacing10 = 10;
 
   static const double spacing12 = 12;
 
@@ -10,6 +14,8 @@ mixin AppDimensions {
   static const double spacing20 = 20;
 
   static const double spacing24 = 24;
+
+  static const double spacing32 = 32;
 
   static const double radius6 = 6;
 
