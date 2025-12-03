1c6bf8ff30ca4ea02fe38903a42552d74855a1a0
Lennon Castro <lennon.castro.92@gmail.com>
2025-12-02 21:21:32 -0300

    feat: add dimensions file

---
+++ lib/core/ds/dimensions.dart
@@ -0,0 +1,15 @@
+mixin AppDimensions {
+  static const double spacing8 = 8;
+
+  static const double spacing16 = 16;
+
+  static const double spacing24 = 24;
+
+  static const double radius6 = 6;
+
+  static const double radius8 = 8;
+
+  static const double icon24 = 24;
+
+  static const double icon32 = 32;
+}
--- lib/core/ds/ds.dart
+++ lib/core/ds/ds.dart
@@ -1,2 +1,3 @@
 export 'colors.dart';
+export 'dimensions.dart';
 export 'theme.dart';
