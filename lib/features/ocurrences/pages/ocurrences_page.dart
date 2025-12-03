026ab7833f3bc0ed4a1bea73c3f8e172fea2666c
Lennon Castro <lennon.castro.92@gmail.com>
2025-12-02 23:43:27 -0300

    feat: apply some dimensions values and labels in ocurrences page

--- lib/features/ocurrences/pages/ocurrences_page.dart
+++ lib/features/ocurrences/pages/ocurrences_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:transport_occurrence/core/ds/ds.dart';
 import 'package:transport_occurrence/core/extensions/labels.dart';
+import 'package:transport_occurrence/gen/assets.gen.dart';
 
-class OcurrencesPage extends StatelessWidget with AppTheme, AppDimensions {
+class OcurrencesPage extends StatelessWidget with AppTheme {
   OcurrencesPage({super.key});
 
   final controller = TextEditingController();
@@ -13,8 +15,9 @@ class OcurrencesPage extends StatelessWidget with AppTheme, AppDimensions {
     return AppPage(
       title: labels.title,
       body: SingleChildScrollView(
-        padding: EdgeInsets.all(24),
+        padding: EdgeInsets.all(AppDimensions.spacing24),
         child: Column(
+          spacing: 16,
           children: [
             AppTextFormField(
               controller: controller,
@@ -27,7 +30,48 @@ class OcurrencesPage extends StatelessWidget with AppTheme, AppDimensions {
                 return null;
               },
             ),
+            Container(
+              constraints: BoxConstraints(maxHeight: 96),
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: 96,
+                  mainAxisExtent: 96,
+                ),
+                itemCount: 1,
+                itemBuilder: (context, index) {
+                  return Container(
+                    constraints: BoxConstraints(maxWidth: 96),
+                    decoration: BoxDecoration(
+                      color: Color(0XFFF6F6FB),
+                      borderRadius: BorderRadius.circular(
+                        AppDimensions.radius8,
+                      ),
+                    ),
+                    padding: EdgeInsets.all(AppDimensions.spacing32),
+                    child: SvgPicture.asset(
+                      Assets.icons.cameraAdd,
+                      width: 32,
+                      height: 32,
+                    ),
+                  );
+                },
+              ),
+            ),
           ],
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(AppDimensions.spacing24),
+        child: AppElevatedButton(
+          label: labels.form.buttonLabel,
+          icon: Padding(
+            padding: EdgeInsets.only(top: AppDimensions.spacing2),
+            child: SvgPicture.asset(
+              Assets.icons.roundArrowRight,
+              width: 20,
+              height: 20,
+            ),
+          ),
         ),
       ),
     );
