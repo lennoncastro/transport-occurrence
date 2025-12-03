b77304fa23f2301206d8983db0bf124aa91be05a
Lennon Castro <lennon.castro.92@gmail.com>
2025-12-02 23:42:51 -0300

    feat: add bottom navigation bar widget in app page

--- lib/core/ds/components/app_page.dart
+++ lib/core/ds/components/app_page.dart
@@ -2,20 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:transport_occurrence/core/ds/ds.dart';
 
 class AppPage extends StatelessWidget with AppTheme, AppDimensions {
-  const AppPage({required this.title, this.body, super.key});
+  const AppPage({
+    required this.title,
+    this.body,
+    this.bottomNavigationBar,
+    this.resizeToAvoidBottomInset = false,
+    super.key,
+  });
 
   final String title;
 
   final Widget? body;
 
+  final Widget? bottomNavigationBar;
+
+  final bool resizeToAvoidBottomInset;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: resizeToAvoidBottomInset,
       appBar: AppBar(
         leading: Navigator.canPop(context) ? AppBarLeading() : null,
         title: Text(title),
       ),
       body: body,
+      bottomNavigationBar: bottomNavigationBar,
     );
   }
 }
