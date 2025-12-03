5040023b72dd8d7d60f14d571af80eeac67aee27
Lennon Castro <lennon.castro.92@gmail.com>
2025-12-03 20:28:18 -0300

    feat: register navigation service in di

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,8 +1,15 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:transport_occurrence/core/navigation/navigator_service.dart';
 import 'package:transport_occurrence/features/checklist/checklist_module.dart';
 import 'package:transport_occurrence/features/ocurrences/ocurrences_module.dart';
 
 class AppModule extends Module {
+  @override
+  void binds(Injector i) {
+    i.addLazySingleton<NavigationService>(NavigationServiceImpl.new);
+    super.binds(i);
+  }
+
   @override
   void routes(r) {
     r.module('/', module: ChecklistModule());
