45445593afec147362394bdd85dde3964a1cb456
Lennon Castro <lennon.castro.92@gmail.com>
2025-12-03 19:18:04 -0300

    feat: add ontTap callback in checklist card

--- lib/features/checklist/components/checklist_card.dart
+++ lib/features/checklist/components/checklist_card.dart
@@ -4,68 +4,80 @@ import 'package:transport_occurrence/core/ds/ds.dart';
 import 'package:transport_occurrence/gen/assets.gen.dart';
 
 class ChecklistCard extends StatelessWidget with AppTheme, AppDimensions {
-  const ChecklistCard({required this.label, required this.icon, super.key});
+  const ChecklistCard({
+    required this.label,
+    required this.icon,
+    this.onTap,
+    super.key,
+  });
 
   final String label;
 
   final Widget icon;
 
+  final VoidCallback? onTap;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 80,
-      decoration: BoxDecoration(
-        color: Color(0xffFFFFFF),
-        borderRadius: BorderRadius.all(Radius.circular(AppDimensions.radius8)),
-        boxShadow: [
-          BoxShadow(
-            color: const Color(0x0D000000),
-            offset: const Offset(0, 2),
-            blurRadius: AppDimensions.radius6,
-            spreadRadius: 0,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 80,
+        decoration: BoxDecoration(
+          color: Color(0xffFFFFFF),
+          borderRadius: BorderRadius.all(
+            Radius.circular(AppDimensions.radius8),
           ),
-        ],
-      ),
-      padding: EdgeInsets.symmetric(
-        horizontal: AppDimensions.spacing24,
-        vertical: AppDimensions.spacing16,
-      ),
-      child: Row(
-        spacing: AppDimensions.spacing16,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            spacing: AppDimensions.spacing16,
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  color: Color(0xffF6F6FB),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(AppDimensions.radius8),
-                  ),
-                ),
-                padding: EdgeInsets.all(AppDimensions.spacing8),
-                child: icon,
-              ),
-              Text(
-                label,
-                style: Theme.of(
-                  context,
-                ).textTheme.bodyMedium!.copyWith(color: Color(0xff515458)),
-              ),
-            ],
-          ),
-          Container(
-            width: AppDimensions.icon24,
-            height: AppDimensions.icon24,
-            padding: EdgeInsets.all(3),
-            child: SvgPicture.asset(
-              Assets.icons.startLarge,
-              width: 6,
-              height: 14,
+          boxShadow: [
+            BoxShadow(
+              color: const Color(0x0D000000),
+              offset: const Offset(0, 2),
+              blurRadius: AppDimensions.radius6,
+              spreadRadius: 0,
             ),
-          ),
-        ],
+          ],
+        ),
+        padding: EdgeInsets.symmetric(
+          horizontal: AppDimensions.spacing24,
+          vertical: AppDimensions.spacing16,
+        ),
+        child: Row(
+          spacing: AppDimensions.spacing16,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              spacing: AppDimensions.spacing16,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xffF6F6FB),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(AppDimensions.radius8),
+                    ),
+                  ),
+                  padding: EdgeInsets.all(AppDimensions.spacing8),
+                  child: icon,
+                ),
+                Text(
+                  label,
+                  style: Theme.of(
+                    context,
+                  ).textTheme.bodyMedium!.copyWith(color: Color(0xff515458)),
+                ),
+              ],
+            ),
+            Container(
+              width: AppDimensions.icon24,
+              height: AppDimensions.icon24,
+              padding: EdgeInsets.all(3),
+              child: SvgPicture.asset(
+                Assets.icons.startLarge,
+                width: 6,
+                height: 14,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
