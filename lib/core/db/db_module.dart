eebc6dd90d8a9424e7ab6bbc8a3cc8f19feafee9
Lennon Castro <lennon.castro.92@gmail.com>
2025-12-04 09:30:01 -0300

    feat: add db module

---
+++ lib/core/db/db_module.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:transport_occurrence/core/db/daos/ocurrence_dao.dart';
+import 'package:transport_occurrence/core/db/db_connection.dart';
+
+class DbModule extends Module {
+  @override
+  void binds(Injector i) {
+    i.addLazySingleton<DatabaseConnection>(DatabaseConnection.new);
+    i.addLazySingleton<OcurrenceDao>(
+      () => OcurrenceDao(i.get<DatabaseConnection>()),
+    );
+    super.binds(i);
+  }
+}
