876dc6b8319f2328610bb55b2c94e90610ae5ccf
Lennon Castro <lennon.castro.92@gmail.com>
2025-12-03 22:35:34 -0300

    chore: remove unused import

--- lib/features/ocurrences/viewmodels/ocurrence_plate_viewmodel.dart
+++ lib/features/ocurrences/viewmodels/ocurrence_plate_viewmodel.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:transport_occurrence/core/camera/camera_provider.dart';
 import 'package:transport_occurrence/core/navigation/navigator_service.dart';
 import 'package:transport_occurrence/features/ocurrences/stores/ocurrence_plate_store.dart';
