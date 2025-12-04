cbed56a6d8ab91e3d32fa005e7a42664a9d6cb68
Lennon Castro <lennon.castro.92@gmail.com>
2025-12-04 09:38:03 -0300

    feat: add core module to app module

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,22 +1,15 @@
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:transport_occurrence/core/camera/camera_provider.dart';
-import 'package:transport_occurrence/core/navigation/navigator_service.dart';
-import 'package:transport_occurrence/core/signature/signature_provider.dart';
+import 'package:transport_occurrence/core/core_module.dart';
 import 'package:transport_occurrence/features/checklist/checklist_module.dart';
 import 'package:transport_occurrence/features/ocurrences/ocurrences_module.dart';
 
 class AppModule extends Module {
   @override
-  void binds(Injector i) {
-    i.addLazySingleton<NavigationService>(NavigationServiceImpl.new);
-    i.addLazySingleton<CameraProvider>(CameraProviderImpl.new);
-    i.addLazySingleton<SignatureProvider>(SignatureProviderImpl.new);
-    super.binds(i);
-  }
-
-  @override
   void routes(r) {
     r.module('/', module: ChecklistModule());
     r.module(OcurrencesModule.name, module: OcurrencesModule());
   }
+
+  @override
+  List<Module> get imports => [CoreModule()];
 }
