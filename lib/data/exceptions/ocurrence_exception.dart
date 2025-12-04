717df46e412ce03a0d577a8cfc3d14157adfb239
Lennon Castro <lennon.castro.92@gmail.com>
2025-12-04 09:45:06 -0300

    feat: add ocurrence especifc exceptions

---
+++ lib/data/exceptions/ocurrence_exception.dart
@@ -0,0 +1,36 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'ocurrence_exception.freezed.dart';
+
+@freezed
+sealed class OcurrenceException with _$OcurrenceException implements Exception {
+  const OcurrenceException._();
+
+  const factory OcurrenceException.notFound({
+    required String message,
+    String? ocurrenceId,
+    Object? originalException,
+  }) = OcurrenceNotFoundException;
+
+  const factory OcurrenceException.alreadyExists({
+    required String message,
+    String? ocurrenceId,
+    Object? originalException,
+  }) = OcurrenceAlreadyExistsException;
+
+  const factory OcurrenceException.creationFailed({
+    required String message,
+    Object? originalException,
+  }) = OcurrenceCreationException;
+
+  const factory OcurrenceException.updateFailed({
+    required String message,
+    String? ocurrenceId,
+    Object? originalException,
+  }) = OcurrenceUpdateException;
+
+  const factory OcurrenceException.queryFailed({
+    required String message,
+    Object? originalException,
+  }) = OcurrenceQueryException;
+}
