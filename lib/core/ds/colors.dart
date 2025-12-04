ca65555c0dc0f9ebbfa669797ee9599594eed4d3
Lennon Castro <lennon.castro.92@gmail.com>
2025-12-04 00:45:55 -0300

    chore: register all project colors

--- lib/core/ds/colors.dart
+++ lib/core/ds/colors.dart
@@ -5,5 +5,19 @@ mixin AppColors {
 
   static const white = Colors.white;
 
+  static const black = Colors.black;
+
   static const background = Color(0xffFEFEFE);
+
+  static const backgroundSecondary = Color(0xffF6F6FB);
+
+  static const textPrimary = Color(0xff515458);
+
+  static const textSecondary = Color(0xff9CA3AF);
+
+  static const border = Color(0xFFE0E6ED);
+
+  static const disabled = Color(0xffE5E7EB);
+
+  static const shadow = Color(0x0D000000);
 }
