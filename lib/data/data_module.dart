f4fac42e4788ca86307e2c07cf635bb2aa89f044
Lennon Castro <lennon.castro.92@gmail.com>
2025-12-04 09:48:48 -0300

    feat: create data module

---
+++ lib/data/data_module.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:transport_occurrence/core/core_module.dart';
+import 'package:transport_occurrence/core/db/daos/ocurrence_dao.dart';
+import 'package:transport_occurrence/data/repositories/ocurrence_repository.dart';
+
+class DataModule extends Module {
+  @override
+  void binds(Injector i) {
+    i.addLazySingleton<OcurrenceRepository>(
+      () => OcurrenceRepositoryImpl(i.get<OcurrenceDao>()),
+    );
+    super.binds(i);
+  }
+
+  @override
+  List<Module> get imports => [CoreModule()];
+}
